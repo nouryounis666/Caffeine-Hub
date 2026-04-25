dae23da846b38f33b7fece2e629720dc84b34cb4
ahmedhanyno <ahmedhanyno@gmail.com>
2026-04-26 01:52:08 +0300

    Implement Swiper for slider functionality
    Added Swiper initialization for slider functionality with pagination and navigation support.

--- js/main.js
+++ js/main.js
@@ -68,3 +68,37 @@ closeBtn.addEventListener("click", () => {
                     modal.style.display = "none";
                 }
             });
+
+
+//sowar section 
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+
+});
