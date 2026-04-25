1c056b46d921b0fbe1252b349e58d287254c044b
shazahossam006-dotcom <shazahossam070@gmail.com>
2026-04-26 00:55:32 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,43 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+/*counter animation */
+const counters = document.querySelectorAll(".counter");
+const section = document.querySelector(".stats");
+
+let started = false;
+
+const startCounting = () => {
+  counters.forEach(counter => {
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+
+      const increment = target / 100;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCounter, 30);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    updateCounter();
+  });
+};
+
+const observer = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting && !started) {
+    startCounting();
+    started = true;
+  }
+});
+
+observer.observe(section);
+
  const modal = document.getElementById("lightbox-modal");
         const fullImg = document.getElementById("full-img");
         const closeBtn = document.querySelector(".close-btn");
