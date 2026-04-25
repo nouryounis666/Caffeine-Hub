0601728d5260723c429972535e6500ca260c535a
roudysabry <roudy.neven@gmail.com>
2026-04-26 00:26:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ window.addEventListener("scroll", function () {
     navbar.classList.remove("scrolled");
   }
 });
+ const modal = document.getElementById("lightbox-modal");
+        const fullImg = document.getElementById("full-img");
+        const closeBtn = document.querySelector(".close-btn");
+        const links = document.querySelectorAll(".light-link");
+
+        links.forEach(link => {
+            link.addEventListener("click", function(event) {
+                event.preventDefault();   
+                const imagePath = this.getAttribute("href"); 
+                fullImg.src = imagePath;
+                modal.style.display = "flex";
+            
+            })
+        })
+closeBtn.addEventListener("click", () => {
+            modal.style.display = "none";
+        });
+
+  window.addEventListener("click", (event) => {
+                if (event.target === modal) {
+                    modal.style.display = "none";
+                }
+            });
