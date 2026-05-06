5f25e058b7984776e56b4256ced4cc430225f58f
ahmedhanyno <ahmedhanyno@gmail.com>
2026-05-07 00:18:50 +0300

    Add contact form and navbar scroll functionality

---
+++ js/contact-us.js
@@ -0,0 +1,57 @@
+/*navbar background*/
+window.addEventListener("scroll", function () {
+  let navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+
+//sowar section 
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+
+});
+
+const contactForm = document.getElementById('sendForm');
+
+if (contactForm) {
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        
+        
+        alert('Your message has been sent successfully!');
+        
+        this.reset();
+    });
+}
